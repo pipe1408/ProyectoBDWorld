05caa66abb5157d4c0ff74a279989efb754872c0
Felipe <pipeba1408@gmail.com>
2022-10-21 16:16:26 -0500

    Estructura

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,6 +1,10 @@
-package PACKAGE_NAME;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApplication extends Application {
 
@@ -9,7 +13,15 @@ public class MainApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("vista/main.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("Consulta Base de Datos World");
+        primaryStage.setResizable(false);
+
+        primaryStage.show();
+
     }
 }
